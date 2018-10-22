c0580919a8debbbc33317dce636813de1fde3621
JaydenBarrie <42758407+JaydenBarrie@users.noreply.github.com>
2018-10-23 11:30:24 +1300

    Jacob in database finally..........
    ww

--- projectDev/projectDev/Models/CourseMarks.cs
+++ projectDev/projectDev/Models/CourseMarks.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace projectDev.Models
 {
@@ -10,6 +11,8 @@ namespace projectDev.Models
         public int ID { get; set; }
         public int StudentId { get; set; }
         public string CourseId { get; set; }
+
+        [Display(Name = "Year Taken")]
         public int Yeartaken { get; set; }
         public int Semester { get; set; }
         public int CourseMark { get; set; }
